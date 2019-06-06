5078ff9b1811ad3ad18e58df65f779547c24cfeb
himangshupal <himangshu.pal1986@gmail.com>
2019-06-06 11:54:03 -0400

    Test if Pychar and Git is working

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\manisha\desktop\dev\mongo-assignment\myvenv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
